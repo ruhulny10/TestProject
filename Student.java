20a829c37568731275d8305ae5582f23a78ce6bf
Ruhul Amin <ruhulny10@gmail.com>
2016-02-24 21:42:22 -0500

    we add two more statement.

--- Student.java
+++ Student.java
@@ -6,16 +6,29 @@ public class Student{
         System.out.println("Hello World");
 	System.out.println("This is my first program");
 	gfshgdjhg
-   }    System.out.println("Welcome to bangladesh");
+       System.out.println("Welcome to bangladesh");
 
         System.out.println("This is our land");
         System.out.println("Hello this mr ruhul amin");
         System.out.println("I am happy now");
         System.out.println("HI how are you");       
+        System.out.println("We like football");
+        System.out.println(" we are student");
+        System.out.println("bangladesh is my land");
+        System.out.println("I write arabic");
+        System.out.println("i loe my land"0;
+        System.out.println("this is nyc");
+        System.out.println("I love flower"):
+        System.out.println("He is good at programming");
+        System.out.println("I know Java");
+        System.out.println("he is mr dk");  
+
+        
 
 
 
-
-
-  }System.out.println("good night shakil");
+ 	System.out.println("good night shakil");
        
+	}
+
+}
